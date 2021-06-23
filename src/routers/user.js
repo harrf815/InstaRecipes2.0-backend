3dc6da617fa79da0d76108096489f26bb3e2300d
Harrf Akbar <harrf_akbar@yahoo.com>
2021-06-23 14:00:06 -0700

    login and profile

--- src/routers/user.js
+++ src/routers/user.js
@@ -22,4 +22,23 @@ router.post('/signup', async (req, res) => {
     }
 })
 
+router.post('/login', async (req, res) => {
+
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+
+        res.send({ user, token })
+
+    } catch (e) {
+        res.status(500).send({ error: 'Unable to find account!'})
+    }
+})
+
+//! GET ----------------------------------
+
+router.get('/profile', auth, async (req, res) => {
+    res.send(req.user)
+})
+
 module.exports = router 
